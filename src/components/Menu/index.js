23d43f74f8dc89291e491ad571c19e51c2394a11
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-06-26 18:22:48 -0600

    FontAwesome
    + Se agrego FontAwesome el proyecto

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,9 +4,14 @@ import './index.css';
 //assets
 import logo from '../../assets/img/logo.png';
 
+//fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 export default function Menu() {
 
-    function myFunction() {
+    function navResponsive() {
         var x = document.getElementById("navbar-nav");
         if (x.className === "navbar-nav") {
             x.className += " show";
@@ -20,7 +25,9 @@ export default function Menu() {
                 <div className="navbar-logo">
                     <a href="#"><img src={logo} alt="logo"/></a>
                 </div>
-                <a className="icon" onClick={myFunction}>X</a>
+                <a className="icon" onClick={navResponsive}>
+                    <FontAwesomeIcon icon={faBars} />
+                </a>
                 <div id="navbar-nav" className="navbar-nav">
                     <a href="#">Characters</a>
                     <a href="#">Locations</a>
